b616e5aa1bde2dea324c7a20744488f8eb181cba
Starmarin <marinastarostenko20@gmail.com>
2021-03-01 20:57:15 +0200

    add files

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service'
+import './random-planet.css';
+
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService()
+  state = {
+    id: null,
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diametr: null
+  };
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25 + 2);
+    this.swapiService.getPlanet(id)
+      .then((planet) => {
+        this.setState({
+          id: id,
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diametr: planet.diameter
+        })
+      })
+  }
+  constructor() {
+    super();
+    this.updatePlanet();
+
+  }
+  render() {
+    const {
+      id,
+      name,
+      population,
+      rotationPeriod,
+      diametr
+    } = this.state;
+    return (
+      <div className="random-planet jumbotron rounded">
+        <img className="planet-image"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population:</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period:</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter:</span>
+              <span>{diametr}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    );
+  }
+}
