cdce1e57505346ce15bb5e1ecdaefd197cf502e9
Starmarin <marinastarostenko20@gmail.com>
2021-03-09 22:30:27 +0200

    Router update

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,35 +2,81 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import ErrorBoundry from '../error-boundry';
+import SwapiService from '../../services/swapi-service';
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage
+} from '../pages';
 
-import ErrorIndicator from '../error-indicator';
-import PeoplePage from '../people-page';
+import { SwapiServiceProvider } from '../swapi-service-context';
+
 import './app.css';
 
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import StarshipDetails from '../sw-components/starship-details';
+
 export default class App extends Component {
+
   state = {
-    hasError: false
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   };
 
-  onPersonSelected = (id) => {
+  onLogin = () => {
     this.setState({
-      selectedPerson: id
+      isLoggedIn: true
     });
   };
-  componentDidCatch() {
-    this.setState({ hasError: true });
-  }
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />
-    }
-    return (
-      <div className="stardb-app">
-        <Header />
-        <RandomPlanet />
 
-        <PeoplePage />
-      </div>
+    const { isLoggedIn } = this.state;
+
+    return (
+      <ErrorBoundry>
+        <SwapiServiceProvider value={this.state.swapiService} >
+          <Router>
+            <div className="stardb-app">
+              <Header />
+              <RandomPlanet />
+
+              <Switch>
+                <Route path="/"
+                  render={() => <h2>Welcome to StarDB</h2>}
+                  exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id"
+                  render={({ match }) => {
+                    const { id } = match.params;
+                    return <StarshipDetails itemId={id} />
+                  }} />
+
+                <Route
+                  path="/login"
+                  render={() => (
+                    <LoginPage
+                      isLoggedIn={isLoggedIn}
+                      onLogin={this.onLogin} />
+                  )} />
+
+                <Route
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn} />
+                  )} />
+
+                <Route render={() => <h2>Sorry, page not found</h2>} />
+              </Switch>
+
+            </div>
+          </Router>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
     );
   }
 }
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,51 +1,80 @@
 import React, { Component } from 'react';
-import SwapiService from '../../services/swapi-service';
-import './random-planet.css';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
+import SwapiService from '../../services/swapi-service';
+
+import './random-planet.css';
+
 export default class RandomPlanet extends Component {
-  swapiService = new SwapiService()
+
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
+  static propTypes = {
+    updateInterval: PropTypes.number
+  };
+
+  swapiService = new SwapiService();
+
   state = {
     planet: {},
-    loading: true,
-    error: false
+    loading: true
   };
+
+  componentDidMount() {
+    const { updateInterval } = this.props;
+    this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, updateInterval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
-      loading: false
+      loading: false,
+      error: false
     });
-  }
+  };
+
   onError = (err) => {
     this.setState({
       error: true,
       loading: false
     });
-  }
+  };
+
   updatePlanet = () => {
-    const id = Math.floor(Math.random() * 17 + 2);
-    this.swapiService.getPlanet(id)
+    const id = Math.floor(Math.random()*17) + 2;
+    this.swapiService
+      .getPlanet(id)
       .then(this.onPlanetLoaded)
-      .catch(this.onError)
-  }
-  componentDidMount() {
-    this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 2500);
-  }
+      .catch(this.onError);
+  };
 
   render() {
     const { planet, loading, error } = this.state;
+
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator/> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading && !error ? <PlanetView planet={planet} /> : null;
-    const err = error ? <ErrorIndicator /> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
+
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
-        {err}
       </div>
     );
   }
+
 }
 
 const PlanetView = ({ planet }) => {
@@ -54,9 +83,10 @@ const PlanetView = ({ planet }) => {
     rotationPeriod, diameter } = planet;
 
   return (
-    <>
+    <React.Fragment>
       <img className="planet-image"
-        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+           alt="planet" />
       <div>
         <h4>{name}</h4>
         <ul className="list-group list-group-flush">
@@ -74,6 +104,6 @@ const PlanetView = ({ planet }) => {
           </li>
         </ul>
       </div>
-    </>
+    </React.Fragment>
   );
 };
